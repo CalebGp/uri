381773c7151376e8a0be79fe48835faceb7402ec
caleb.gp.dev <caleb.gp.dev@gmail.com>
2024-08-05 02:19:56 +0000

    doing 1145

--- 1145/1145.py
+++ 1145/1145.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split())
-aux = 1
 for i in range(1, m, n):
-    for i in range(n):
-        print(aux+i, end=" ")
-    aux += 4
+    aux = ""
+    for j in range(n):
+       aux += str(i+j)
+    print(aux)
